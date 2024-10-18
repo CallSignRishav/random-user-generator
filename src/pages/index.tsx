050fa3eb2a215466692154738d493f47ecd014df
Rishav Mondal <mondalrishav25@gmail.com>
2024-10-18 11:47:46 +0530

    favicon & head updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import FetchData from "@/components/Application/FetchData";
 import RefetchButton from "@/components/Application/RefetchButton";
+import Head from "next/head";
 
 const index = () => {
   return (
     <>
+      <Head>
+        <title>Random User Generator</title>
+        <meta name="description" content="Random User Generator" />
+      </Head>
+
       <div className="grid place-items-center py-10 px-8 space-y-4">
         <RefetchButton />
 
